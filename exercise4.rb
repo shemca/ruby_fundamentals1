b37e55d8d655cdcc92765d03cee45666f1c9408f
Shem <shemca@gmail.com>
2015-03-31 15:21:35 -0400

    First draft of exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,11 @@
+count = (1..100)
+
+if count = count / 3 == 0
+	puts "Bit"
+	elseif count = count / 5 == 0
+	puts "Maker"
+	elseif count = count / 5 == 0 && count / 3 == 0
+	puts "Bitmaker"
+	else
+	puts "#{count}"
+	end	
