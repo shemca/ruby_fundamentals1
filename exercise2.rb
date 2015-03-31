4d49e459965fde06a8466a76716b93ed1857c025
Shem <shemca@gmail.com>
2015-03-31 14:43:31 -0400

    Second Exercise Completed

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# First part of the exercise
+x = 55.00 * 1.15
+puts " The total for a 55 dollar meal with a 15% tip would be $#{x.round(2)}."
+
+# Second part of the exercise
+y = "Braps Claps" + 44.to_s
+puts y
+
+#Third part of the exercise
+z = 45628 * 7839
+puts " The total of 45628 times 7839 is #{z}."
+
+#Fourth part of the exercise
+
+a = (true && false) || (false && true) || !(false && false)
+puts a
